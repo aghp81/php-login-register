e5428c4f20e6f41341c04e06777a3bd5794a1ee3
aghp81 <aghp81@gmail.com>
2025-04-17 07:23:59 +0330

    post table

--- db.sql
+++ db.sql
@@ -19,3 +19,13 @@ CREATE TABLE password_resets (
 ALTER TABLE users ADD role ENUM('admin', 'editor', 'user') DEFAULT 'user';
 
 composer require phpmailer/phpmailer
+
+
+CREATE TABLE posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
